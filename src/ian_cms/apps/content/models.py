cb6fd11ea229961ddd40d7ee757140ade9aba985
iankinyua-impactafrica <ian@impactafrica.network>
2022-08-04 21:42:32 +0300

    Transferred content into apps

---
+++ src/ian_cms/apps/content/models.py
@@ -0,0 +1,16 @@
+from operator import mod
+from pickle import TRUE
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+
+class ContentCategory(models.Model):
+    category_id = models.IntegerField(primary_key=TRUE)
+    category_name = models.CharField(max_length=20)
+    category_id = models.CharField(max_length=20)
+
+
+class ContentType(models.Model):
+    ContentCategory = models.ForeignKey(on_delete=models.DO_NOTHING)
+    
