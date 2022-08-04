2f539ebefb154ae7164bcfbe00ec02be28109a3e
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-08-04 23:18:03 +0300

    Add views for ptow and articles

--- src/ian_cms/apps/content/views.py
+++ src/ian_cms/apps/content/views.py
@@ -8,4 +8,16 @@ class Home(View):
     def get(self, request):
         return render(request, 'index.html', {})
 
+class Firesides(View):
+    def get(self, request):
+        return render(request, 'fireside-videos.html', {})
+
+class PTOW(View):
+    def get(self, request):
+        return render(request, 'ptow.html', {})
+
+class Articles(View):
+    def get(self, request):
+        return render(request, 'articles.html', {})        
+
         
